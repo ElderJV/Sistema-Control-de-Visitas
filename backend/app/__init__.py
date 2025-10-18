ec0e3f954062ad30aca291b9074c67ebf7857752
ElderJV <elderjulcavelasco@gmail.com>
2025-10-18 02:25:21 -0500

    feature(documentation): Add documentation for last changes

--- backend/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from .models.usuario import Usuario
-from .models.visita import Visita
-from .models.visitante import Visitante
-from .models.autorizaciones import Autorizacion
-from .models.vetados import Vetado
-
-__all__ = ["Usuario", "Visita", "Visitante", "Autorizacion", "Vetado"]
